3690ea89bad6ca644e59438bd27a52f968071703
Shubham <sm.phoujdar@gmail.com>
2021-03-19 08:10:39 +0530

    Added : Email pattern matching.

--- src/main/java/regexUserRegistration.java
+++ src/main/java/regexUserRegistration.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -24,20 +25,37 @@ public class regexUserRegistration {
     public static void main(String[] args) {
 
         String firstNameRegExpPattern = "^[A-Z][a-z]{2,}";
-        String firstNameString;
-        firstNameString = getUserInput(scanner, "Please Enter Valid First Name:");
+        String firstNameString = getUserInput(scanner, "Please Enter Valid First Name:");
         if(validatePattern(firstNameString, firstNameRegExpPattern)){
             System.out.println("First Name String is Valid.");
         }else
             System.out.println("First Name String is invalid.");
 
         String lastNameRegExpPattern = "^[A-Z][a-z]{2,}";
-        String lastNameString;
-        lastNameString = getUserInput(scanner, "Please Enter Valid Last Name:");
+        String lastNameString = getUserInput(scanner, "Please Enter Valid Last Name:");
+
         if(validatePattern(lastNameString, lastNameRegExpPattern)){
             System.out.println("Last Name String is Valid.");
         }else
             System.out.println("Last Name String is invalid.");
+
+
+        String emailRegExpPattern = "^([A-Za-z0-9]+[\\.\\+-]?[A-Za-z0-9]+)+[\\@][a-zA-Z]+[\\.][a-zA-Z]{2,3}[\\.]?[a-zA-Z]{0,2}";
+        String[] emailList= {"emailabc@yahoo.com" ,"abc-100@yahoo.com" ,"abc.100@yahoo.com", "abc111@abc.com",
+                             "a.b1.00@abc.net", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com",
+                             "abc+100@gmail.com", "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com",
+                             "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com",
+                             "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"};
+
+
+        for (int i = 0; i< emailList.length; i++) {
+            String emailString = emailList[i];
+            if (validatePattern(emailString, emailRegExpPattern)) {
+                System.out.printf("Valid : %s\n",emailList[i]);
+            } else
+                System.out.printf("Invalid : %s\n",emailList[i]);
+        }
+
 
 
     }
