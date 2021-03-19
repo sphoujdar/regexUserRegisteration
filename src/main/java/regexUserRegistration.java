d4194d930323fef885b52452d54e45c9951accb0
Shubham <sm.phoujdar@gmail.com>
2021-03-19 06:59:56 +0530

    Added : First name pattern matching.

--- src/main/java/regexUserRegistration.java
+++ src/main/java/regexUserRegistration.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class regexUserRegistration {
 
-    public static void main(String[] args) {
-        System.out.println("We will solve Regex Problems here.");
+    //Some Variables used throughout the program
+    static Scanner scanner = new Scanner(System.in);
+
+
+
+    public static String getUserInput(Scanner sc, String message){
+        System.out.printf("%s\n",message);
+        return sc.next();
     }
+
+    public static boolean validatePattern(String argument, String regExp){
+        Pattern pattern = Pattern.compile(regExp);
+        Matcher matcher = pattern.matcher(argument);
+        return matcher.matches();
+    }
+
+
+    public static void main(String[] args) {
+
+        String firstNameRegExpPattern = "^[A-Z][a-z]{2,}";
+        String firstNameString;
+        firstNameString = getUserInput(scanner, "Please Enter Valid First Name:");
+
+        if(validatePattern(firstNameString, firstNameRegExpPattern)){
+            System.out.println("First Name String is Valid.");
+        }else
+            System.out.println("First Name String is invalid.");
+
+
+    }
+
+
+
 
 }
